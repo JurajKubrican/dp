5bfe9a6fdc7a36a975cc7bee105fbf2eccbc0f75
theyurry <kubrican.juraj@gmail.com>
2019-04-15 13:34:27 +0200

    kill script

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-
+fuser -k 7777/tcp
+fuser -k 8080/tcp
+fuser -k 8081/tcp
+fuser -k 8082/tcp
+fuser -k 8083/tcp
+fuser -k 8088/tcp
 
 
 ./gradlew -b ./eureka/build.gradle  bootrun &
+sleep 5
 ./gradlew -b ./authorization/build.gradle  bootrun &
+sleep 5
 ./gradlew -b ./gateway/build.gradle  bootrun &
+sleep 5
 ./gradlew -b ./generator/build.gradle  bootrun &
